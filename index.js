47eac4a04e82044e94147f2428f627eb46053c41
Mahima <98544175+mahisrivastava1218@users.noreply.github.com>
2023-02-11 23:15:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+// import{x} from "./hello"
+// const hello=require('./hello')
+
+
+// var x=20
+// const arr=[2,4,5,3,7,9,3,8]
+// console.log(arr)
+// console.log(hello.z())
+// for(i=0;i<5;i++){
+//      console.log(i)
+// }
+// arr.filter(item=>{
+//     console.log(item)
+
+// })
+
+// let result=arr.filter(item=>{
+//        return item===3;
+// })
+// let result=arr.filter(item=>{
+//     return item>=3;
+// })
+// console.log(result)
+// const fs=require('fs')
+// console.log("code step by step")
+
+// fs.writeFileSync("hello takila.txt","code for takila")
+
+// const fs=require('fs')
+// console.log("->> see my file",__filename)
+// fs.writeFileSync("hello.txt","I'm not your bff")
+
+// // const fs=require('fs')
+// console.log("->",__dirname)
+// fs.writeFileSync("hello.txt","I'm not your bff")
+
+// const gs=require('fs').writeFileSync
+// gs("vtou.txt","u don't hit me")
+ 
+
+
+// make basic server
+
+// const http=require('http')
+// http.createServer().listen(4000);
+
+
+// const http=require('http')
+// http.createServer((req,resp)=>{
+//     resp.write("hello i'm your dorame")
+//     resp.end()
+// }).listen(4000)
+
+// const http=require('http')
+// function datacon(req,resp){
+//     resp.write("hello i'm your dorame")
+//     resp.end()
+// }
+// http.createServer(datacon).listen(4000)
+
+
+// const http=require('http')
+// const datacon=(req,res)=>
+// {
+//     res.write("hello i'm your dorame");
+//     res.end();
+// }
+//     http.createServer(datacon).listen(4000)
+
+// const colors=require('colors')
+// console.log("hello dorame".blue)
+
+//Nodemon  saving module
+
+console.log("Hello dorame");
